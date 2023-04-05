f93a1d49a59c06674df178651b7609f0bdc1ba15
Isauro Avila <isauro@avila-ti.com>
2023-04-05 12:27:04 -0600

    144. Navigator y el contexto

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:nav_dialog/main.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const MenuPage(),
+              ),
+            );
+          },
+          child: const Text('Back to Menu'),
+        ),
+      ),
+    );
+  }
+}
